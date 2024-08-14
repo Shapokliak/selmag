d88b5e5535c61b2850bf4c417f40ebc6d98c6da8
Shapokliak <sunshinesout@yandex.ru>
2024-08-14 20:35:49 +0300

    Basic Authentification

--- manager-app/src/main/java/ag/selm/manager/config/ClientBeans.java
+++ manager-app/src/main/java/ag/selm/manager/config/ClientBeans.java
@@ -4,15 +4,20 @@ import ag.selm.manager.client.RestClientProductsRestClientImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientBeans {
 
     @Bean
-    public RestClientProductsRestClientImpl productsRestClient(@Value("${selmag.services.catalogue.url:http://localhost:8081}") String catalogueBaseUri) {
+    public RestClientProductsRestClientImpl productsRestClient(
+            @Value("${selmag.services.catalogue.url:http://localhost:8081}") String catalogueBaseUri,
+            @Value("${selmag.services.catalogue.username:}") String catalogueUserName,
+            @Value("${selmag.services.catalogue.password:}") String cataloguePassword) {
         return new RestClientProductsRestClientImpl(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUserName, cataloguePassword))
                 .build());
     }
 }
